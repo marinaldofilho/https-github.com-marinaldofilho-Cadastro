b6a17bf98b4c6a87c0b6397c0aabb6c09eb5cdf0
Marinaldo Filho <Marinaldo Filho@10.13.250.2>
2015-06-19 07:47:39 -0300

    Primeiro commit

---
+++ src/dao/UsuarioDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import factory.ConnectionFactory;
+import modelo.Usuario;
+import java.sql.*;
+import java.sql.PreparedStatement;
+
+public class UsuarioDAO {
+    private Connection connection;
+    Long id;
+    String nome;
+    String cpf;
+    String email;
+    String telefone;
+    
+    public UsuarioDAO(){
+        this.connection = new ConnectionFactory().getConnection();
+    }
+    
+    public void adiciona(Usuario usuario){
+        String sql = "INSERT INTO usuario(nome,cpf,email,telefone) VALUES(?,?,?,?)";
+        try {
+         PreparedStatement stmt = connection.prepareStatement(sql);   
+         stmt.setString(1, usuario.getNome());
+         stmt.setString(2, usuario.getCpf());
+         stmt.setString(3, usuario.getEmail());
+         stmt.setString(1, usuario.getTelefone());
+         stmt.execute();
+         stmt.close();
+        
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+    }
+    
+}
